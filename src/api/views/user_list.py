15f27d6705d2475345f0b2717625fa51d325226e
a85274220-ai <a85274220@gmail.com>
2026-03-02 16:22:29 +0500

    add: all source codes

---
+++ src/api/views/user_list.py
@@ -0,0 +1,13 @@
+from rest_framework.viewsets import ModelViewSet
+from apps.users.models import User
+from rest_framework.permissions import AllowAny
+from api.seralizers.user_app import UserListSeralizers
+from api.paginations import MyCustomPaginator
+
+
+class UserModelViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserListSeralizers
+    permission_classes = [AllowAny]
+    pagination_class = MyCustomPaginator
+
